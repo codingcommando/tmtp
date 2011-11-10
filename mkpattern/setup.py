a807421ba5e7952350ca32d5bdf3336aa9c08372
Steve Conklin <steve@conklinhouse.com>
2011-11-09 20:33:47 -0600

    changes to setup.py

--- mkpattern/setup.py
+++ mkpattern/setup.py
@@ -1,5 +1,10 @@
 from distutils.core import setup
 setup(name='mkpattern',
       version='1.0',
-      py_modules=['tmtpl'],
+      description='mkpattern, part of the openstitch project',
+      author='Steve Conklin',
+      author_email='steve@conklinhouse.com',
+      url='http://www.openstitch.org/',
+      scripts=['mkpattern'],
+      packages=['tmtpl']
       )
